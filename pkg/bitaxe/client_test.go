c2674ca13af41e142efd79ccf22c5bbdd03d9970
Adam Shannon <adamkshannon@gmail.com>
2025-01-03 10:20:47 -0600

    test: fix compile after refactor

--- pkg/bitaxe/client_test.go
+++ pkg/bitaxe/client_test.go
@@ -10,14 +10,14 @@ import (
 )
 
 var (
-	baseAddress = strings.TrimSpace(os.Getenv("BITAXE_BASE_ADDRESS"))
+	defaultBaseAddress = strings.TrimSpace(os.Getenv("BITAXE_BASE_ADDRESS"))
 )
 
 func TestClient_SystemInfo(t *testing.T) {
 	c := testClient(t)
 
 	ctx := context.Background()
-	info, err := c.SystemInfo(ctx)
+	info, err := c.SystemInfo(ctx, baseAddress(t))
 	require.NoError(t, err)
 
 	require.NotEmpty(t, info.BestDiff)
@@ -26,9 +26,19 @@ func TestClient_SystemInfo(t *testing.T) {
 func testClient(t *testing.T) Client {
 	t.Helper()
 
-	if baseAddress == "" {
+	if testing.Short() {
+		t.Skip("-short flag provided")
+	}
+
+	return NewClient(nil)
+}
+
+func baseAddress(t *testing.T) string {
+	t.Helper()
+
+	if defaultBaseAddress == "" {
 		t.Skip("BITAXE_BASE_ADDRESS is empty")
 	}
 
-	return NewClient(baseAddress, nil)
+	return defaultBaseAddress
 }
